9a938f811883f2f4eb555fc1be0014cc91ee31e5
makolon <s.makolon@icloud.com>
2021-01-12 13:06:04 +0900

    update music.py

--- catkin_ws/src/navigation/src/music.py
+++ catkin_ws/src/navigation/src/music.py
@@ -4,17 +4,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy.linalg as LA
 import math
+import sys
 
 class Music(object):
-    def __init__(self):
-        self.data = pd.real_csv("csvPath")
+    # TODO
+    #   - adjust the number of self.res, which means resolution.
+    #   - adjust csv file in order to load file 5 seconds.
+    def __init__(self, csvPath, number):
+        self.data = pd.read_csv(csvPath, engine='python')
+        self.data = self.data.drop(columns='Unnamed: 0')
         self.M = 2
-        self.n = 100
+        self.n = number
         self.res = 1000
         self.X = []
 
     def calc_R(self):
-        data = self.data.ix[0:self.n, :]
+        # print(self.data)
+        data = self.data.iloc[0:self.n, :]
         self.X = signal.hilbert(data.T)
         self.X = self.X.T
         R = [[0 for i in range(self.M)] for j in range(self.M)]
@@ -23,7 +29,7 @@ class Music(object):
             for j in range(self.M):
                 R_ij = 0
                 for k in range(self.n):
-                    R_ij += self.X[k, i] * np.comjugate(self.X[k, j])
+                    R_ij += self.X[k, i] * np.conjugate(self.X[k, j])
                 R[i][j] = R_ij / self.n
         return R
 
@@ -36,30 +42,42 @@ class Music(object):
 
         for x in range(self.res):
             sin = math.sin(start)
-            a = np.empty(10, dtype=complex)
-            for i in range(10):
+            a = np.empty(self.M, dtype=complex)
+            for i in range(self.M):
                 s = pow(np.e, (complex(0, -(i+1)*np.pi)*sin))
                 a[i] = s
-            a_el = a.reshape((1, 10))
+            a_el = a.reshape((1, self.M))
             a_el = np.conjugate(a_el)
             E_el = np.conjugate(E.T)
             numerator = np.dot(a_el, a)
             b_1 = np.dot(a_el, E)
             b_2 = np.dot(b_1, E_el)
-            denominator = np.dot(b_2, a)
-            music_spec[x] = 10 * (np.log10(numerator.real/denominator.real))
+            denominator = np.dot(b_2, a) + 0.1
+            music_spec[x] = self.M * (np.log10(numerator.real/denominator.real))
             start += div
         return music_spec
 
+    def likelihood_sound_direction(self, music_spec):
+        max_num = 0
+        for i in range(len(music_spec)):
+            if music_spec[i] > max_num:
+                max_num = music_spec[i]
+        music_direction = max_num
+        return music_direction
+
     def plot_x(self, music_spec):
-        x_axis = np.linspace(-90, 90, self.res):
+        x_axis = np.linspace(-90, 90, self.res)
         plt.plot(x_axis, music_spec)
         plt.xlabel("Angle[angle]")
         plt.ylabel("Amplitude[db]")
         plt.show()
 
 if __name__ == "__main__":
-    music = Music()
+    file_path = '/home/makolon/turtlebot2clap/catkin_ws/src/navigation/src/sample_Output_stereo.csv'
+    number = 1128791
+    music = Music(file_path, number)
     R = music.calc_R()
     spec = music.calc_spec(R)
-    music.plot_x(spec)
+    direction = music.likelihood_sound_direction(spec)
+    print(direction)
+    # music.plot_x(spec)
--- catkin_ws/src/navigation/src/wav_to_csv.py
+++ catkin_ws/src/navigation/src/wav_to_csv.py
@@ -11,18 +11,29 @@ samrate, data = wavfile.read(str('./wavfile/' + input_filename))
 print('Load is Done! \n')
 
 wavData = pd.DataFrame(data)
-
+"""
 if len(wavData.columns) == 2:
     print('Stereo .wav file\n')
     wavData.columns = ['R', 'L']
     stereo_R = pd.DataFrame(wavData['R'])
     stereo_L = pd.DataFrame(wavData['L'])
+    print(stereo_R)    
     print('Saving...\n')
     stereo_R.to_csv(str(input_filename[:-4] + "_Output_stereo_R.csv"), mode='w')
     stereo_L.to_csv(str(input_filename[:-4] + "_Output_stereo_L.csv"), mode='w')
     # wavData.to_csv("Output_stereo_RL.csv", mode='w')
     print('Save is done ' + str(input_filename[:-4]) + '_Output_stereo_R.csv , '
                           + str(input_filename[:-4]) + '_Output_stereo_L.csv')
+"""
+if len(wavData.columns) == 2:
+    print('Stereo .wav file\n')
+    wavData.columns = ['R', 'L']
+    print(wavData)
+    # stereo_R = pd.DataFrame(wavData['R'])
+    # stereo_L = pd.DataFrame(wavData['L'])
+    # wavData = pd.merge(stereo_R, stereo_L, on='R', how='left')
+    wavData.to_csv(str(input_filename[:-4] + "_Output_stereo.csv"), mode='w')
+    print('Save id done ' + str(input_filename[:-4]) + '_Output_stereo.csv')
 
 elif len(wavData.columns) == 1:
     print('Mono .wav file\n')
@@ -38,4 +49,3 @@ else:
     wavData.to_csv(str(input_filename[:-4] + "Output_multi_channel.csv"), mode='w')
 
     print('Save is done ' + str(input_filename[:-4]) + 'Output_multi_channel.csv')
-
